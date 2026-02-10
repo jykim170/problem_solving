8b386cf5fb89b6a214f8cb7afc9a9aff7f325d22
jykim170 <vertoco@naver.com>
2026-02-10 15:10:46 +0900

    [Bronze V] Title: 구구단, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2739. 구구단/구구단.py
@@ -0,0 +1,12 @@
+# a, b, c, d, e = map(int, input().split())
+
+# # print(n//m)
+# # print(n%m)
+
+a = int(input())
+# b = int(input())
+
+# print((a**2 + b**2 + c**2+ d**2+ e**2)%10)
+
+for i in range(1, 10):
+    print(f'{a} * {i} = {a*i}')
