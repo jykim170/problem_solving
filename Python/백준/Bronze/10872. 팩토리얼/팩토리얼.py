62e91298f7ddce4d8537229373ed5d9be7f62035
jykim170 <vertoco@naver.com>
2026-02-10 14:05:39 +0900

    [Bronze III] Title: 팩토리얼, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -0,0 +1,11 @@
+def factorial(num):
+    if num == 0:
+        return 1
+    elif num == 1:
+        return 1
+    return num * factorial(num-1)
+
+N = int(input())
+
+result = factorial(N)
+print(result)
