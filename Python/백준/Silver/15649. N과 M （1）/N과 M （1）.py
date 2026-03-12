46a97c42fd0a68754811991ab3aa1d069db0834a
jykim170 <vertoco@naver.com>
2026-03-12 14:35:27 +0900

    [Silver III] Title: N과 M (1), Time: 176 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ Python/백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,17 +1,17 @@
 def dfs(idx):
     if idx == m:
         print(*arr)
-        return
-    
+        return    
+
     for i in range(1, n+1):
-        if not visited[i]:
+        if visited[i] == 0:
             visited[i] = True
             arr[idx] = i
-            dfs(idx+1)
+            dfs(idx + 1)
             visited[i] = False
-    
+
 n, m = map(int, input().split())
-arr = [0] * m
-visited = [False] * (n+1)
+arr = [0] * m 
+visited =[0] * (n+1)
 
 dfs(0)
